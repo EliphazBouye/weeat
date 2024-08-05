49d11455a7e4492c9b84c2808d082bcef4bf6667
Sophonie Bouye <sophoniebouye@gmail.com>
2024-08-05 12:05:46 +0000

    core: add and configure swager ui

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const configService = app.get(ConfigService);
+  const configSwagger =  new DocumentBuilder()
+    .setTitle('WeEat')
+    .setDescription('The WEEAT API Documentation')
+    .setVersion('0.1')
+    .addTag('weeat')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('api', app, document);
   await app.listen(configService.get<number>('PORT'));
 }
 bootstrap();
