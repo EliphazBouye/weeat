e01abdce29338ce0fbf9009c7e6575e142b3e1d8
Sophonie Bouye <sophoniebouye@gmail.com>
2024-08-05 11:29:15 +0000

    core: create prisma module, create user module with service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
 import configuration from './config/configuration';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
     load: [configuration],
-  })],
+  }), UsersModule, PrismaModule],
 })
 export class AppModule {}
